864c144d37d2a4a6b5c31fba5bf4891582733e9f
aahamidan.dev <aahamidan.dev@gmail.com>
2025-01-01 16:31:33 +0700

    part-3-router-arif init post & user router

--- router/postRoute.js
+++ router/postRoute.js
@@ -1,46 +1,176 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+//
+const router = express.Router();
 
 const postData = [
-    {
-        judul: 'satu',
-        konten: 'asfgakjsgfkajsfgjkashfk'
-    },
-    
-    {
-        judul: 'sambilan',
-        konten: 'asfgakjsgfkajsfgjkashfk'
-    },
-    {
-        judul: 'sasak',
-        konten: 'asfgakjsgfkajsfgjkashfk'
-    }
-]
+  {
+    judul: "satu",
+    konten: "lorem ipsum dolor sit amet tempor invid",
+  },
+  {
+    judul: "salad",
+    konten: "lorem ipsum dolor sit amet tempor invid",
+  },
+  {
+    judul: "tiga",
+    konten: "lorem ipsum dolor sit amet tempor invid",
+  },
+];
 
-// get semua post
-router.get('/post', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: postData
-    })
-})
+// //get semua post dan get by query param judul
+router.get("/post", (req, res) => {
+  const { judul } = req.query;
+  if (judul) {
+    const cari = postData.filter((cariJudul) =>
+      cariJudul.judul.toLowerCase().includes(judul.toLowerCase())
+    );
 
-router.post('/post', (req, res) => {
-    const {judul, konten} = req.body
-
-    // cek judul sudaha da atau gak
-    const postExist = postData.find(x => x.judul === judul)
-    if (postExist) {
-        res.status(409).json({
-            status: 'failed',
-            message: 'Judul sudah terpakai',
-        })
+    if (cari.length === 0) {
+      return res.status(400).json({
+        status: "failed",
+        message: "data not found",
+      });
     } else {
-        postData.push({judul, konten})
-        res.status(200).json({
-            status: 'success',
-        })
+      return res.status(200).json({
+        status: "success",
+        data: cari,
+      });
     }
-})
+  } else {
+    return res.status(200).json({
+      status: "success",
+      data: postData,
+    });
+  }
+});
 
-module.exports = router
+//get data by query parameters with filter
+router.get("/post/search", (req, res) => {
+  const { judul } = req.query;
+
+  if (!judul)
+    return res.status(400).json({
+      status: "failed",
+      message: "Judul yang anda cari harus diisi",
+    });
+
+  const cari = postData.filter((cariJudul) => cariJudul.judul.includes(judul));
+
+  if (cari.length === 0)
+    return res.status(404).json({
+      status: "failed",
+      message: "Post dengan judul yang anda cari tidak ditemukan",
+    });
+
+  console.log(cari);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      ...cari,
+    },
+  });
+});
+
+//get finding post with req params
+router.get("/post/:judul", (req, res) => {
+  const cari = postData.find((aJudul) => aJudul.judul === req.params.judul);
+  if (!cari) {
+    return res.status(404).json({
+      status: "failed",
+      message: "Post not found",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      judul: cari.judul,
+      konten: cari.konten,
+    },
+  });
+});
+
+router.delete("/post", (req, res) => {
+  const { judul } = req.query;
+
+  if (!judul)
+    return res.status(400).json({
+      status: "failed",
+      message: "Judul yang anda ingin hapus harus diisi",
+    });
+
+  const index = postData.findIndex((post) => post.judul === judul);
+
+  if (index === -1)
+    return res.status(404).json({
+      status: "failed",
+      message: "post tidak ditemukan",
+    });
+
+  postData.splice(index, 1);
+  res.status(200).json({
+    status: "success",
+    message: "Judul berhasil di hapus",
+  });
+});
+
+router.put("/post", (req, res) => {
+  //const { judul} = req.query;
+  const { judul, konten } = req.body;
+
+  //check title is available or not
+  const postExist = postData.find((arr) => arr.judul === judul);
+  // console.log(postData.title);
+  console.log(postExist);
+  // console.log(judul);
+  if (!postExist) {
+    res.status(400).json({
+      status: "failed",
+      message: "Judul post tidak ada",
+    });
+    return;
+  }
+  //update post
+  const postIndex = postData.findIndex((post) => post.judul === judul);
+
+  postData[postIndex] = { ...postData[postIndex], konten };
+
+  console.log(postExist);
+  res.status(200).json({
+    status: "success",
+    message: "Post berhasil diupdate",
+    data: postExist,
+  });
+});
+
+router.post("/post", (req, res) => {
+  const { judul, konten } = req.body;
+
+  //check title is available or not
+  const postExist = postData.find((arr) => arr.judul === judul);
+  // console.log(postData.title);
+  // console.log(postExist);
+  // console.log(judul);
+
+  if (postExist) {
+    res.status(400).json({
+      status: "failed",
+      message: "Judul post sudah ada",
+    });
+    return;
+  }
+
+  //add new post to array data post
+  postData.push({ judul, konten });
+  // console.log ({judul, konten});
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      judul,
+      konten,
+    },
+  });
+});
+
+module.exports = router;
---
+++ router/user.js
@@ -0,0 +1,129 @@
+const express = require("express");
+
+const app = express();
+
+const router = express.Router();
+
+const userData = [
+  {
+    nama: "satu",
+    umur: 10,
+  },
+  {
+    nama: "dua",
+    umur: 20,
+  },
+  {
+    nama: "tiga",
+    umur: 30,
+  },
+  {
+    nama: "salwa",
+    umur: 26,
+  },
+];
+
+router.get("/user", (req, res) => {
+  const { nama } = req.query;
+
+  if (nama) {
+    const cari = userData.filter((n) =>
+      n.nama.toLowerCase().includes(nama.toLowerCase())
+    );
+    if (cari.length === 0) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+    } else {
+      return res.status(200).json({
+        status: "success",
+        data: cari,
+      });
+    }
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: userData,
+  });
+});
+
+router.delete("/user", (req, res) => {
+  const { nama } = req.query;
+
+  if (!nama)
+    return res.status(400).json({
+      status: "failed",
+      message: "nama user yang anda ingin hapus harus diisi",
+    });
+
+  const index = userData.findIndex((user) => user.nama === nama);
+
+  if (index === -1)
+    return res.status(404).json({
+      status: "failed",
+      message: "user tidak ditemukan",
+    });
+
+  userData.splice(index, 1);
+  res.status(200).json({
+    status: "success",
+    message: "User berhasil di hapus",
+  });
+});
+
+router.put("/user", (req, res) => {
+  const { nama, umur } = req.body;
+  //check title is available or not
+  const userExist = userData.find((arr) => arr.nama === nama);
+  console.log(userExist);
+
+  if (!userExist) {
+    res.status(400).json({
+      status: "failed",
+      message: "User tidak ada",
+    });
+    return;
+  }
+  //update user
+  const userIndex = userData.findIndex((user) => user.nama === nama);
+
+  userData[userIndex] = { ...userData[userIndex], umur };
+
+  console.log(userExist);
+  res.status(200).json({
+    status: "success",
+    message: "User data berhasil diupdate",
+    data: userExist,
+  });
+});
+
+router.post("/user", (req, res) => {
+  const { nama, umur } = req.body;
+
+  //check title is available or not
+  const userExist = userData.find((arr) => arr.nama === nama);
+
+  if (userExist) {
+    res.status(400).json({
+      status: "failed",
+      message: "nama sudah ada",
+    });
+    return;
+  }
+
+  //add new user to array data user
+  userData.push({ nama, umur });
+  // console.log ({User, konten});
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      nama,
+      umur,
+    },
+  });
+});
+
+module.exports = router;
